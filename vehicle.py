575b7cb3284c4e809e655b5985a60438dd888f55
derm1811-gif <derm1811@gmail.com>
2026-08-27 08:18:22 -0500

    Add vehicle module

---
+++ vehicle.py
@@ -0,0 +1,22 @@
+class vehicle:
+    def __init__(self,brand,color,plate):
+        self.brand=brand
+        self.color=color
+        self.plate=plate
+        self.speed=0
+        
+    def acelerar(self):
+        self.speed+=10
+        print(f"el{self.brand} acelero a {self.speed}km/h")
+        
+    def desacelerar(self):
+        self.speed-=10
+        print(f"el{self.brand} desacelero a {self.speed}km/h")
+
+#Creación de los objetos
+my_vehicle=vehicle("ferrari","verde","ABC312")
+my_vehicle.acelerar()
+my_vehicle.acelerar()
+my_vehicle.acelerar()
+my_vehicle.desacelerar()
+my_vehicle.desacelerar()
